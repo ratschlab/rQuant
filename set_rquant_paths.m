1df68339c2aed83cc0c479625b58538fc8e2e8a7
bohnert <bohnert@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2010-06-02 17:25:37 +0000

    separated config for dirs, data, paths from rquant parameters - moved path settings to set_rquant_paths - moved dir/data settings to run_rquant - created run_rquant_template - created run_rquant_data_sim.m for data simulation settings - configure_rquant now only contains paramaters relevant for rquant

--- set_rquant_paths.m
+++ set_rquant_paths.m
@@ -1,6 +1,31 @@
-function rquant_paths = set_rquant_paths()
-% rquant_paths = set_rquant_paths()
+function rquant_paths = set_rquant_paths(CFG)
+% rquant_paths = set_rquant_paths(CFG)
 
-rquant_paths = '~/svn/tools/genomes:~/svn/tools/utils:~/svn/tools/ngs';
+%%%%% genome utils %%%%%
+rquant_paths = '~/svn/tools/genomes:~/svn/tools/utils:~/svn/tools/ngs:~/svn/projects/genefinding/utils:';
+
+%%%%% rproc %%%%%
+if CFG.use_rproc
+  p = '~/svn/tools/rproc';
+  rquant_paths = sprintf('%s:%s', p, rquant_paths);
+end
+
+%%%%% optimizer %%%%%
+p = '/fml/ag-raetsch/share/software/matlab_tools/cplex9';
+rquant_paths = sprintf('%s:%s', p, rquant_paths);
+addpath(p);
+switch CFG.optimizer
+ case 'cplex'
+  p = '/fml/ag-raetsch/share/software/matlab_tools/cplex9';
+  rquant_paths = sprintf('%s:%s', p, rquant_paths);
+ case 'mosek'
+  p = '/fml/ag-raetsch/share/software/mosek/6/toolbox/r2009b';
+  rquant_paths = sprintf('%s:%s', p, rquant_paths);
+  if CFG.use_rproc
+    CFG.rproc_par.envstr = 'export MOSEKLM_LICENSE_FILE=/fml/ag-raetsch/share/software/mosek/6/licenses/mosek.lic; export LD_LIBRARY_PATH=/fml/ag-raetsch/share/software/mosek/6/tools/platform/linux64x86/bin';
+  end
+ otherwise
+  error('unknown optimizer %s', CFG.optimizer);
+end
 
 addpath(rquant_paths);
