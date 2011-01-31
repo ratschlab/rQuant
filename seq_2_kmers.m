53319e53ff3fb8d31b733983b86b8a7cd3860bbd
bohnert <bohnert@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2011-01-31 17:12:23 +0000

    faster implementation of getting k-mers features from a sequence

--- seq_2_kmers.m
+++ seq_2_kmers.m
@@ -4,57 +4,59 @@
 % the Free Software Foundation; either version 3 of the License, or
 % (at your option) any later version.
 %
-% Written (W) 2007-2010 Georg Zeller, Regina Bohnert, Gunnar Raetsch
+% Written (W) 2007-2010 Regina Bohnert, Gunnar Raetsch
 % Copyright (C) 2007-2010 Max Planck Society
 %
 
-function kmers = seq_2_kmers(seq, order)
-% kmers = seq_2_kmers(seq, order)
+function kmers = seq_2_kmers(seq, order, win_size)
+% kmers = seq_2_kmers(seq, order, win_size)
 %
 % -- input --
-% seq: matrix of sequences
+% seq: sequence
 % order: maximal substring length
+% win_size: size of subsequence window
 %
 % -- output --
 % kmers: matrix of positional substring occurrence
 
 
 assert(all(all(seq=='A' | seq=='C' | seq=='G' | seq=='T')));
-s_len = size(seq,1);
-offset = 0;
+s_len = length(seq);
 acgt = 'ACGT';
 num_nt = length(acgt);
-for o = 1:order,
-  offset(o+1) = offset(o) + (s_len-o+1) * num_nt^o;
-end
-kmer_dim = offset(end);
 
-nucleotides{1} = acgt';
+% determine number of k-mer occurences in whole sequence
+offset_all = 0;
+for o = 1:order,
+  offset_all(o+1) = offset_all(o) + num_nt^o;
+end
+kmer_dim = offset_all(end);
+
+kmers_all = zeros(kmer_dim, s_len);
+for n = 1:num_nt,
+  kmers_all(n,:) = double(seq==acgt(n));
+end  
 for o = 2:order,
-  nucleotides{o} = char(zeros(length(nucleotides{o-1}) * num_nt, o));
-  for s = 1:length(nucleotides{o-1}),
-    for n = 1:num_nt,
-      row = (s-1) * num_nt + n;
-      nucleotides{o}(row,:) = [nucleotides{o-1}(s,:) acgt(n)];
-    end
+  for n = offset_all(o-1)+1:offset_all(o),
+    kmers_all(offset_all(o)+(n-offset_all(o-1)-1)*num_nt+[1:num_nt],1:end-o+1) = repmat(kmers_all(n,1:end-o+1), num_nt, 1) .* kmers_all(1:num_nt,o:end);
   end
 end
 
-kmers = zeros(kmer_dim, size(seq,2));
+% save number of k-mer occurences per subsequence for each position
+offset = 0;
 for o = 1:order,
-  for s = 1:s_len-o+1,
-    for n = 1:length(nucleotides{o});
-      idx = strmatch(nucleotides{o}(n,:), seq(s:s+o-1,:)');
-      row = offset(o) + (s-1)*num_nt^o + n;
-      kmers(row,idx) = 1;
-    end
+  offset(o+1) = offset(o) + (win_size-o+1) * num_nt^o;
+end
+kmer_dim = offset(end);
+kmers = zeros(kmer_dim, length(seq)-win_size);
+for n = win_size/2+1:length(seq)-win_size/2,
+  for o = 1:order,
+    kmers(offset(o)+1:offset(o+1),n-win_size/2) = reshape(kmers_all(offset_all(o)+1:offset_all(o+1), n-win_size/2:n+win_size/2-1-o+1), (win_size-o+1) * num_nt^o, 1);
   end
 end
 
 exp_sum = 0;
 for o = 1:order,
-  exp_sum = exp_sum + s_len-o+1;
+  exp_sum = exp_sum + win_size-o+1;
 end
 assert(all(sum(kmers) == exp_sum));
-
-
