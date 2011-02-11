59b066bd1b62b9fa04ec323e4138b6908a572306
bohnert <bohnert@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2011-02-11 11:22:11 +0000

    separate calculation of averaged number of read starts (background)

--- gen_sequence_features.m
+++ gen_sequence_features.m
@@ -1,4 +1,4 @@
-function [X Y] = gen_sequence_features(CFG, gene, tscp_idx)
+function [X Y Y_bg] = gen_sequence_features(CFG, gene, tscp_idx)
 % [X Y] = gen_sequence_features(CFG, gene, tscp_idx)
 %
 % -- input --
@@ -9,6 +9,7 @@ function [X Y] = gen_sequence_features(CFG, gene, tscp_idx)
 % -- output --
 % X: matrix of positional substring occurence (number of k-mers x positions)
 % Y: vector of target values for regression
+% Y_bg: vector of averaged number of read starts (background)
 
 exons = gene.exons{tscp_idx};
 eidx = [];
@@ -29,12 +30,11 @@ seq = upper(load_genomic(gene.chr, gene.strands(tscp_idx), exons(:,1), exons(:,2
 X = seq_2_kmers(seq, CFG.RR.order, 2*CFG.RR.half_win_size);
 
 % target values for regression
-if nargout==2
+if nargout>1
   num_read_starts = gene.num_read_starts(idx2);
   assert(length(seq)==length(num_read_starts)+2*CFG.RR.half_win_size);
-  num_read_starts_norm = length(num_read_starts) * num_read_starts / sum(num_read_starts);
-  Y = log(num_read_starts_norm + 1e-5)';
+  %num_read_starts_norm = length(num_read_starts) * num_read_starts / sum(num_read_starts);
+  Y_bg = repmat(log(median(num_read_starts) + 1e-10), 1, length(num_read_starts));
+  Y = log(num_read_starts + 1e-10)'-Y_bg;
   assert(size(X,2)==length(Y));
 end
-
-
