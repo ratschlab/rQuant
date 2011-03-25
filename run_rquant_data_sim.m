9705233c6e1197773f812ba62a5eaa7517398640
Regina Bohnert <Regina_Bohnert@web.de>
2011-03-25 11:29:52 +0000

    looping over different regularisation parameters for job submission

--- run_rquant_data_sim.m
+++ run_rquant_data_sim.m
@@ -30,19 +30,9 @@ CFG.tracks_max_intron_len = 1e9;
 CFG.tracks_min_exon_len = -1;
 CFG.tracks_max_mismatches = 1e3;
 
-%%%%% result directory %%%%%
-date_exp = '2011-01-29';
-%date_exp = datestr(now,'yyyy-mm-dd');
-%date_exp = datestr(now,'yyyy-mm-dd_HHhMM');
-CFG.out_dir = sprintf('/fml/ag-raetsch/share/projects/rquant/data_sim/elegans/WS200/rquant/%s_%s/', CFG.exp, date_exp);
-if ~exist(CFG.out_dir ,'dir'),
-  [s m mid] = mkdir(CFG.out_dir);
-  assert(s);
-end
-
 %%%%% output files %%%%%
-CFG.write_gff = 1;
-CFG.write_density_model = 1;
+CFG.write_gff = 0;
+CFG.write_density_model = 0;
 
 %%%%% optimizer %%%%%
 CFG.optimizer = 'cplex';
@@ -51,7 +41,12 @@ CFG.optimizer = 'cplex';
 CFG.learn_profiles = 1;
 
 %%%%% pre-learned profiles %%%%%
-CFG.profiles_fn = '/fml/ag-raetsch/share/projects/rquant/data_sim/elegans/WS200/rquant/fs_strong_bias_2011-01-26/profiles.mat';
+CFG.profiles_fn = '';
+
+%%%%% regularisation strengths %%%%%
+C_I = [10^0 10^1 10^2];
+C_F = [10^1 10^3 10^4];
+C_N = [10^0 10^1 10^2];
 
 %%%%% rproc settings for rquant subjobs %%%%%
 CFG.use_rproc = 0; % 1: cluster submission or 0: locally
@@ -71,23 +66,42 @@ if CFG.use_rproc,
   CFG.rproc_time                  = 5*60; % mins
 end
 
-CFG
+run_local = 0;
 
-%%%%% rproc settings for main job %%%%%
-if 0
-  rproc_memreq                = 8000;
-  rproc_par.priority          = 8;
-  rproc_par.express           = 0;
-  rproc_par.immediately_bg    = 0;
-  rproc_par.immediately       = 0;
-  rproc_par.arch              = 64;
-  rproc_par.identifier        = '';
-  rproc_par.verbosity         = 0;
-  rproc_time                  = 72*60; % mins
-  rproc_par.envstr            = 'export MOSEKLM_LICENSE_FILE=/fml/ag-raetsch/share/software/mosek/6/licenses/mosek.lic; export LD_LIBRARY_PATH=/fml/ag-raetsch/share/software/mosek/6/tools/platform/linux64x86/bin';
-  rproc_par.identifier = sprintf('rq.%s-', CFG.organism(1:2));
-  fprintf(1, 'Submitting job %s to cluster\n', rproc_par.identifier);
-  job = rproc('rquant', CFG, rproc_memreq, rproc_par, rproc_time);
-else
-  rquant(CFG);
+for s = 1:length(C_I),
+  CFG.C_I = C_I(s);
+  for f = 1:length(C_F),
+    CFG.C_F = C_F(f);
+    for n = 1:length(C_N),
+      CFG.C_N = C_N(n);
+      exp_str = sprintf('s%if%in%i', CFG.C_I, CFG.C_F, CFG.C_N);
+      %%%%% result directory %%%%%
+      %date_exp = '2011-03-02';
+      date_exp = datestr(now,'yyyy-mm-dd');
+      %date_exp = datestr(now,'yyyy-mm-dd_HHhMM');
+      CFG.out_dir = sprintf('/fml/ag-raetsch/share/projects/rquant/data_sim/elegans/WS200/rquant/%s_%s_%s/', CFG.exp, date_exp, exp_str);
+      if ~exist(CFG.out_dir ,'dir'),
+        [s m mid] = mkdir(CFG.out_dir);
+        assert(s);
+      end
+      %%%%% rproc settings for main job %%%%%
+      if ~run_local
+        rproc_memreq                = 2000;
+        rproc_par.priority          = 8;
+        rproc_par.express           = 0;
+        rproc_par.immediately_bg    = 0;
+        rproc_par.immediately       = 0;
+        rproc_par.arch              = 64;
+        rproc_par.identifier        = '';
+        rproc_par.verbosity         = 0;
+        rproc_time                  = 72*60; % mins
+        %rproc_par.envstr            = 'export MOSEKLM_LICENSE_FILE=/fml/ag-raetsch/share/software/mosek/6/licenses/mosek.lic; export LD_LIBRARY_PATH=/fml/ag-raetsch/share/software/mosek/6/tools/platform/linux64x86/bin';
+        rproc_par.identifier = sprintf('rq.%s%s-', CFG.organism(1:2), exp_str);
+        fprintf(1, 'Submitting job %s to cluster\n', rproc_par.identifier);
+        job = rproc('rquant', CFG, rproc_memreq, rproc_par, rproc_time);
+      else
+        rquant(CFG);
+      end
+    end
+  end
 end
