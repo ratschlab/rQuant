babb675141d2216587f37afc7c7938220f4301b8
bohnert <bohnert@e1793c9e-67f9-0310-80fc-b846ff1f7b36>
2011-08-16 06:20:53 +0000

    integrated loss for paired-end data

--- opt_transcripts_descent.m
+++ opt_transcripts_descent.m
@@ -1,7 +1,7 @@
-function [weights, obj, fval] = opt_transcripts_descent(CFG, coverage, exon_mask, intron_count, intron_mask, C_w, R_const, max_iter, weights0, reg)
+function [weights, obj, fval] = opt_transcripts_descent(CFG, coverage, exon_mask, intron_count, intron_mask, paired_exp, paired_obs, C_w, R_const, max_iter, weights0, reg)
 % OPT_TRANSCRIPTS_DESCENT   Determines the optimal transcript weights.
 %
-%   [weights, obj, fval] = opt_transcripts_descent(CFG, coverage, exon_mask, intron_count, intron_mask, C_w, R_const, max_iter, weights0, reg)
+%   [weights, obj, fval] = opt_transcripts_descent(CFG, coverage, exon_mask, intron_count, intron_mask, paired_exp, paired_obs, C_w, R_const, max_iter, weights0, reg)
 %
 %   -- input --
 %   CFG:          configuration struct
@@ -9,6 +9,8 @@ function [weights, obj, fval] = opt_transcripts_descent(CFG, coverage, exon_mask
 %   exon_mask:    PxT matrix modelling 'importance' of a position within a transcript
 %   intron_count: vector of observed intron confirmation
 %   intron_mask:  IxT matrix defining whether an intron belongs to a particular transcript
+%   paired_exp:   matrix (#segments x #segments x T) of connectivity by expected paired-end reads
+%   paired_obs:   matrix (#segments x #segments) of connectivity by observed paired-end reads
 %   C_w:          regularisation parameter per transcript (T x 1)
 %   R_const:      constant residue
 %   max_iter:     maximal number of iterations (optional)
@@ -16,9 +18,9 @@ function [weights, obj, fval] = opt_transcripts_descent(CFG, coverage, exon_mask
 %   reg:          regularisation method (optional)
 %
 %   -- output --
-%   weights:     weights of transcripts
-%   obj:         objective value at optimum
-%   fval:        objective value at each step
+%   weights:      weights of transcripts
+%   obj:          objective value at optimum
+%   fval:         objective value at each step
 %
 %
 %   This program is free software; you can redistribute it and/or modify
@@ -33,18 +35,19 @@ function [weights, obj, fval] = opt_transcripts_descent(CFG, coverage, exon_mask
 
 T = size(exon_mask,2); % number of transcripts
 I = size(intron_mask,1); % number of introns
+PE = size(paired_exp,1); % number of segments
 
 exon_count = sum(coverage,2);
 
-if nargin<7
+if nargin<9
   R_const = 0;
 end
 
-if nargin<8
+if nargin<10
   max_iter = 1e100;
 end
 
-if nargin<9
+if nargin<11
   weights = full(mean(coverage)/T*ones(1,T));
 else
   weights = weights0;
@@ -53,7 +56,7 @@ weights_old = zeros(1,T);
 fval = 1e100*ones(1,T);
 fval_old = zeros(1,T);
 
-if nargin<10
+if nargin<12
   reg = 'L1';
 end
 
@@ -64,35 +67,54 @@ cnt = 0;
 if CFG.VERBOSE>0, fprintf(1, '\nStarting optimising...\n'); tic; end
 if CFG.VERBOSE>1, fprintf(1, 'Itn\tObjective\tNorm diff\n'); end
 if T==1
+  t = 1;
+  % exon loss
   RE = -exon_count;
-  RI = -intron_count;
+  S1 = sum(exon_mask(:,t).^2);
+  S2 = 2*sum(exon_mask(:,t)'*RE);
+  S3 = sum(RE.^2) + R_const;
+  % intron loss
+  if I>0
+    RI = -intron_count;
+    S1 = S1 + CFG.C_I*sum(intron_mask(:,t).^2);
+    S2 = S2 + 2*CFG.C_I*sum(intron_mask(:,t)'*RI);
+    S3 = S3 + CFG.C_I*sum(RI.^2);
+  end
+  % paired loss
+  if PE>0
+    RPE = -paired_obs;
+    S1 = S1 + CFG.C_PE*sum(sum((squeeze(paired_exp(:,:,t))).^2));
+    S2 = S2 + 2*CFG.C_PE*sum(sum(squeeze(paired_exp(:,:,t)).*RPE));
+    S3 = S3 + CFG.C_PE*sum(sum(RPE.^2));
+  end
+  % regularisation
   switch reg
    case 'L1'
-    S1 = sum(exon_mask(:,1).^2); if I>0, S1 = S1 + CFG.C_I*sum(intron_mask(:,1).^2); end
-    S2 = 2*sum(exon_mask(:,1)'*RE) + C_w(1); if I>0, S2 = S2+2*CFG.C_I*sum(intron_mask(:,1)'*RI); end
-    S3 = sum(RE.^2) + R_const; if I>0, S3 = S3 + CFG.C_I*sum(RI.^2); end
+    S2 = S2 + C_w(t);
    case 'L2'
-    S1 = sum(exon_mask(:,1).^2) + C_w(1); if I>0, S1 = S1 + CFG.C_I*sum(intron_mask(:,1).^2); end
-    S2 = 2*sum(exon_mask(:,1)'*RE); if I>0, S2 = S2+2*CFG.C_I*sum(intron_mask(:,1)'*RI); end
-    S3 = sum(RE.^2) + R_const; if I>0, S3 = S3 + CFG.C_I*sum(RI.^2); end
+    S1 = S1 + C_w(t);
   end
   w_new = -0.5*S2/S1;
   % clipping of w_t
   if w_new < 0
-    weights(1) = LB;
+    weights(t) = LB;
   else
-    weights(1) = w_new;
+    weights(t) = w_new;
   end
-  fval(1) = quad_fun(weights(1), S1, S2, S3);
+  fval(t) = quad_fun(weights(t), S1, S2, S3);
   obj_alt = sum((exon_mask*weights'-coverage).^2) + R_const;
   if I>0, obj_alt = obj_alt + CFG.C_I*sum((intron_mask*weights'-intron_count).^2); end
+  if PE>0
+    paired_exp_wsum = squeeze(paired_exp(:,:,t))*weights(t);
+    obj_alt = obj_alt + CFG.C_PE*sum(sum((paired_exp_wsum-paired_obs).^2));
+  end
   switch reg
    case 'L1'
     obj_alt = obj_alt + abs(weights*C_w);
    case 'L2'
     obj_alt = obj_alt + weights.^2*C_w;
   end
-  if ~(abs(fval(1)-obj_alt)<1e-3) % objective should be indentical to not-expanded objective
+  if ~(abs(fval(t)-obj_alt)<1e-3) % objective should be indentical to not-expanded objective
     cnt = cnt + 1;
     if CFG.VERBOSE>1, fprintf(1, 'objectives differ %.6f (tscp %i)\n', abs(fval(t)-obj_alt), t); end
   end
@@ -103,17 +125,36 @@ else
     fval_old = fval;
     for t = 1:T,
       idx_wo_t = setdiff(1:T,t);
+      % exon loss
       RE = exon_mask(:,idx_wo_t)*weights(idx_wo_t)'-exon_count;
-      if I>0, RI = intron_mask(:,idx_wo_t)*weights(idx_wo_t)'-intron_count; end
+      S1 = sum(exon_mask(:,t).^2);
+      S2 = 2*sum(exon_mask(:,t)'*RE);
+      S3 = sum(RE.^2) + R_const;
+      % intron loss
+      if I>0
+        RI = intron_mask(:,idx_wo_t)*weights(idx_wo_t)'-intron_count;
+        S1 = S1 + CFG.C_I*sum(intron_mask(:,t).^2);
+        S2 = S2 + 2*CFG.C_I*sum(intron_mask(:,t)'*RI);
+        S3 = S3 + CFG.C_I*sum(RI.^2);
+      end
+      % paired loss
+      if PE>0
+        RPE = -paired_obs;
+        for tt = idx_wo_t,
+          RPE = RPE + squeeze(paired_exp(:,:,tt))*weights(tt);
+        end
+        S1 = S1 + CFG.C_PE*sum(sum((squeeze(paired_exp(:,:,t))).^2));
+        S2 = S2 + 2*CFG.C_PE*sum(sum(squeeze(paired_exp(:,:,t)).*RPE));
+        S3 = S3 + CFG.C_PE*sum(sum(RPE.^2));
+      end
+      % regularisation
       switch reg
        case 'L1'
-        S1 = sum(exon_mask(:,t).^2); if I>0, S1 = S1 + CFG.C_I*sum(intron_mask(:,t).^2); end
-        S2 = 2*sum(exon_mask(:,t)'*RE) + C_w(t); if I>0, S2 = S2+2*CFG.C_I*sum(intron_mask(:,t)'*RI); end
-        S3 = sum(RE.^2) + R_const + abs(weights(idx_wo_t))*C_w(idx_wo_t); if I>0, S3 = S3 + CFG.C_I*sum(RI.^2); end
+        S2 = S2 + C_w(t);
+        S3 = S3 + abs(weights(idx_wo_t))*C_w(idx_wo_t);
        case 'L2'
-        S1 = sum(exon_mask(:,t).^2) + C_w(t); if I>0, S1 = S1 + CFG.C_I*sum(intron_mask(:,t).^2); end
-        S2 = 2*sum(exon_mask(:,t)'*RE); if I>0, S2 = S2+2*CFG.C_I*sum(intron_mask(:,t)'*RI); end
-        S3 = sum(RE.^2) + R_const + weights(idx_wo_t).^2*C_w(idx_wo_t); if I>0, S3 = S3 + CFG.C_I*sum(RI.^2); end
+        S1 = S1 + C_w(t);
+        S3 = S3 + weights(idx_wo_t).^2*C_w(idx_wo_t);
       end
       w_new = -0.5*S2/S1;
       % clipping of w_t
@@ -125,6 +166,13 @@ else
       fval(t) = quad_fun(weights(t), S1, S2, S3);
       obj_alt = sum((exon_mask*weights'-coverage).^2) + R_const;
       if I>0, obj_alt = obj_alt + CFG.C_I*sum((intron_mask*weights'-intron_count).^2); end
+      if PE>0,
+        paired_exp_wsum = zeros(PE, PE);
+        for tt = 1:T,
+          paired_exp_wsum = paired_exp_wsum + squeeze(paired_exp(:,:,tt))*weights(tt);
+        end
+        obj_alt = obj_alt + CFG.C_PE*sum(sum((paired_exp_wsum-paired_obs).^2));
+      end
       switch reg
        case 'L1'
         obj_alt = obj_alt + abs(weights*C_w);
